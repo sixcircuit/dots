5d807f581111e185fef66d6a369f4c2f5ac91011
Kendrick Taylor <kendrick@circuitsix.com>
2017-11-22 01:16:15 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+cd ~
+ln -s ./shell/ackrc ./.ackrc
+ln -s ./shell/bash_profile ./.bash_profile
+ln -s ./shell/bashrc ./.bashrc
+ln -s ./shell/bin ./bin
+
+cp ./shell/gitconfig.example ./shell/gitconfig
+ln -s ./shell/gitconfig ./.gitconfig
+
+ln -s ./shell/gitignore ./.gitignore
+
+cp ./shell/load_keys.example.sh ./shell/load_keys.sh
+ln -s ./shell/load_keys.sh ./.load_keys.sh
+
+ln -s ./shell/screenrc ./.screenrc
+ln -s ./shell/tmux.conf ./.tmux.conf
+cp ./shell/tux.example.js ./shell/.tux.js
+ln -s ./shell/tux.js ./.tux.js
+
+mkdir ./.vim
+ln -s ./shell/vim/* ./.vim/
+
+ln -s ./shell/vimrc ./.vimrc
+ln -s ./shell/zshrc ./.zshrc
