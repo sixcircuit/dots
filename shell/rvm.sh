a78122b36baf54116ca9ab2d2b2a70c419d29611
Kendrick Taylor <kendrick@circuitsix.com>
2019-11-27 22:59:20 -0800

    lazy load nvm and rvm. speed up zsh launch by a LOT. like 1.5s to 250ms

--- shell/rvm.sh
+++ shell/rvm.sh
@@ -1,4 +1,18 @@
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# direct way
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# lazy load rvm
+
+load_rvm() {
+  unset -f rvm > /dev/null 2>&1
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+  # Load RVM into a shell session *as a function*
+}
+
+rvm() {
+  load_rvm 
+  rvm $@
+}
