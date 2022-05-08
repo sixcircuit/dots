a2c5ecd164ee41d9ef510f16de8b142eec5400b4
Kendrick Taylor <kendrick@circuitsix.com>
2022-05-07 23:27:30 -0400

    update macports install script

--- install_ports.sh
+++ install_ports.sh
@@ -1,8 +1,8 @@
 mkdir ${HOME}/macports_install
 cd ${HOME}/macports_install
-mv ~/Downloads/MacPorts-2.4.2.tar.gz ./
-tar -xzvf MacPorts-2.4.2.tar.gz
-cd MacPorts-2.4.2
+mv ~/Downloads/MacPorts-2.7.2.tar.gz ./
+tar -xzvf MacPorts-2.7.2.tar.gz
+cd MacPorts-2.7.2
 ./configure --prefix=${HOME}/.macports --with-tclpackage=${HOME}/.macports/tcl \
 --with-install-user=${USER} --with-install-group=staff
 make
